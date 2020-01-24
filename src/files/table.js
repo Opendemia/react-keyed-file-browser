803ca0f1dce6a460f093fecf9f6fac9fb955bd99
Nate Sardo <sardo.9@osu.edu>
2020-01-24 18:52:45 -0500

    styled checkbox

--- src/files/table.js
+++ src/files/table.js
@@ -1,48 +1,59 @@
-import React from 'react'
-import Moment from 'moment'
-import ClassNames from 'classnames'
-import { DragSource, DropTarget } from 'react-dnd'
-import { NativeTypes } from 'react-dnd-html5-backend'
+import React from "react";
+import Moment from "moment";
+import ClassNames from "classnames";
+import { DragSource, DropTarget } from "react-dnd";
+import { NativeTypes } from "react-dnd-html5-backend";
 
-import BaseFile, { BaseFileConnectors } from './../base-file.js'
-import { fileSize } from './utils.js'
+import BaseFile, { BaseFileConnectors } from "./../base-file.js";
+import { fileSize } from "./utils.js";
 
 class RawTableFile extends BaseFile {
   render() {
     const {
-      isDragging, isDeleting, isRenaming, isOver, isSelected,
-      action, url, browserProps, connectDragPreview,
-      depth, size, modified, used, course
-    } = this.props
+      isDragging,
+      isDeleting,
+      isRenaming,
+      isOver,
+      isSelected,
+      action,
+      url,
+      browserProps,
+      connectDragPreview,
+      depth,
+      size,
+      modified,
+      used,
+      course
+    } = this.props;
 
-    const icon = browserProps.icons[this.getFileType()] || browserProps.icons.File
-    const inAction = (isDragging || action)
+    const icon =
+      browserProps.icons[this.getFileType()] || browserProps.icons.File;
+    const inAction = isDragging || action;
 
-    let name
+    let name;
     if (!inAction && isDeleting) {
       name = (
         <form className="deleting" onSubmit={this.handleDeleteSubmit}>
           <a
-            href={url || '#'}
+            href={url || "#"}
             download="download"
             onClick={this.handleFileClick}
           >
             {icon}
-            
           </a>
           <div>
-            <button type="submit">
-              Confirm Deletion
-            </button>
+            <button type="submit">Confirm Deletion</button>
           </div>
         </form>
-      )
+      );
     } else if (!inAction && isRenaming) {
       name = (
         <form className="renaming" onSubmit={this.handleRenameSubmit}>
           {icon}
           <input
-            ref={el => { this.newNameRef = el }}
+            ref={el => {
+              this.newNameRef = el;
+            }}
             type="text"
             value={this.state.newName}
             onChange={this.handleNewNameChange}
@@ -50,78 +61,77 @@ class RawTableFile extends BaseFile {
             autoFocus
           />
         </form>
-      )
+      );
     } else {
       name = (
-        <a
-          href={url || '#'}
-          download="download"
-          onClick={this.handleFileClick}
-        >
+        <a href={url || "#"} download="download" onClick={this.handleFileClick}>
           {icon}
           {this.getName()}
         </a>
-      )
+      );
     }
 
-    let draggable = (
-      <div>
-        {name}
-      </div>
-    )
-    if (typeof browserProps.moveFile === 'function') {
-      draggable = connectDragPreview(draggable)
+    let draggable = <div>{name}</div>;
+    if (typeof browserProps.moveFile === "function") {
+      draggable = connectDragPreview(draggable);
     }
 
     let row = (
       <tbody>
         <tr
-          className={ClassNames('file', {
+          className={ClassNames("file", {
             pending: action,
             dragging: isDragging,
             dragover: isOver,
-            selected: isSelected,
+            selected: isSelected
           })}
           onClick={this.handleItemClick}
           onDoubleClick={this.handleItemDoubleClick}
         >
           <td className="name">
-            <div style={{ paddingLeft: (depth * 16) + 'px' }}>
-              {draggable}
-            </div>
+            <div style={{ paddingLeft: depth * 16 + "px" }}>{draggable}</div>
           </td>
           <td>
-          <input 
-            type="checkbox" 
-            name="used"
-            checked={this.props.used}
-            onChange={this.handleUsedEdit}
-          ></input>
+            <div class="fac fac-checkbox-o fac-success ">
+              <span></span>
+              <input
+                id="box-example"
+                type="checkbox"
+                name="used"
+                value="1"
+                style="display: none;"
+                checked={this.props.used}
+                onChange={this.handleUsedEdit}
+              ></input>
+              <label for="box-example">
+              </label>
+            </div>
+            
           </td>
         </tr>
-        {isSelected && this.props.course ?
+        {isSelected && this.props.course ? (
           <tr>
-            <td>
-            {this.props.course}
-            </td>
+            <td>{this.props.course}</td>
           </tr>
-          :
-          null
-        }
+        ) : null}
       </tbody>
-    )
+    );
 
-    return this.connectDND(row)
+    return this.connectDND(row);
   }
 }
 
-@DragSource('file', BaseFileConnectors.dragSource, BaseFileConnectors.dragCollect)
+@DragSource(
+  "file",
+  BaseFileConnectors.dragSource,
+  BaseFileConnectors.dragCollect
+)
 @DropTarget(
-  ['file', 'folder', NativeTypes.FILE],
+  ["file", "folder", NativeTypes.FILE],
   BaseFileConnectors.targetSource,
-  BaseFileConnectors.targetCollect,
+  BaseFileConnectors.targetCollect
 )
 class TableFile extends RawTableFile {}
 
-export default TableFile
-export { RawTableFile }
+export default TableFile;
+export { RawTableFile };
